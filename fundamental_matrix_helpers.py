5f3888544db698968b960b1cfcd94efd404fea2b
Richard Du <rdu@princeton.edu>
2016-11-12 17:01:39 -0500

    Update calculation method for pseudoinverse

--- fundamental_matrix_helpers.py
+++ fundamental_matrix_helpers.py
@@ -10,7 +10,8 @@ def skew_symmetric_matrix (a):
 def compute_fundamental(p1, p2):
     p = np.mat(p1)
     pprime = np.mat(p2)
-    pinv = p.getT()*np.linalg.inv(p*p.getT())
+    pinv = np.linalg.pinv(p)
+    # pinv = p.getT()*np.linalg.inv(p*p.getT())
     pperp = (np.eye(4)-pinv*p)*np.mat('1; 1; 1; 1')
     skew = skew_symmetric_matrix(pprime * pperp)
     F = skew*pprime*pinv
