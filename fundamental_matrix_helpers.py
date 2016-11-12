c87a2a6b1f347c2f02b364f94bdf682fb6c5bb29
Richard Du <rdu@princeton.edu>
2016-11-12 12:54:17 -0500

    Refactor fundamental matrix helpers

--- fundamental_matrix_helpers.py
+++ fundamental_matrix_helpers.py
@@ -1,22 +1,17 @@
 import numpy as np
 
 
-def skewSymmetricMatrix (a):
+def skew_symmetric_matrix (a):
     return np.matrix([[0, -a[2], a[1]],
                       [a[2], 0, -a[0]],
                       [-a[1], a[0], 0]])
 
 
-def computeFundamentalMatrix(p1, p2):
+def compute_fundamental(p1, p2):
     p = np.mat(p1)
     pprime = np.mat(p2)
     pinv = p.getT()*np.linalg.inv(p*p.getT())
     pperp = (np.eye(4)-pinv*p)*np.mat('1; 1; 1; 1')
-    skew = skewSymmetricMatrix(pprime*pperp)
+    skew = skew_symmetric_matrix(pprime * pperp)
     F = skew*pprime*pinv
     return F
-
-
-p1 = np.asarray([[1, 0, 30, 0], [0, 1, 0, 0], [0, 0, 1, 0]])
-p2 = np.asarray([[10, 0, 0, 0], [0, 12, 0, 0], [0, 0, 3, 0]])
-computeFundamentalMatrix(p1, p2)
