dbc592c3c00638cd1ae2223cfca021565a1d5811
Richard Du <rdu@princeton.edu>
2016-11-12 22:51:16 -0500

    Refactor and comment code

--- fundamental_matrix_helpers.py
+++ fundamental_matrix_helpers.py
@@ -1,18 +1,28 @@
 import numpy as np
 
 
-def skew_symmetric_matrix (a):
+def skew_symmetric_matrix(a):
+    """
+    Returns skew matrix that represents a cross product
+    :param a: 3D vector
+    :return: 3x3 skew matrix
+    """
     return np.matrix([[0, -a[2], a[1]],
                       [a[2], 0, -a[0]],
                       [-a[1], a[0], 0]])
 
 
 def compute_fundamental(p1, p2):
+    """
+    Computes fundamental matrix given 2 camera matrices
+    :param p1: 3x4 camera projection matrix
+    :param p2: 3x4 camera projection matrix
+    :return: 3x3 fundamental matrix
+    """
     p = np.mat(p1)
-    pprime = np.mat(p2)
-    pinv = np.linalg.pinv(p)
-    # pinv = p.getT()*np.linalg.inv(p*p.getT())
-    pperp = (np.eye(4)-pinv*p)*np.mat('1; 1; 1; 1')
-    skew = skew_symmetric_matrix(pprime * pperp)
-    F = skew*pprime*pinv
+    p_prime = np.mat(p2)
+    p_inv = np.linalg.pinv(p)
+    p_null = (np.eye(4)-p_inv*p)*np.mat('1; 1; 1; 1')
+    skew = skew_symmetric_matrix(p_prime * p_null)
+    F = skew*p_prime*p_inv
     return F
